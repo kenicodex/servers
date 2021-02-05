f6a8b1a25cacffa789e6eb511ba65ee980eabd65
kehinde <kehindesalaudeen222@gmail.com>
2021-02-05 09:58:14 +0100

    frontend

--- app.js
+++ app.js
@@ -10,14 +10,17 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get("/", (req,res)=>{
+  res.sendFile("/frontend/build/index.html")
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
