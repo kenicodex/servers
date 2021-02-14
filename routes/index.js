a1df43d7e39349df53f29f47e834145d83d204be
kehinde <kehindesalaudeen222@gmail.com>
2021-02-14 07:51:59 +0100

    os type check

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'General Server' });
+  res.render('index', { title: 'General Server, your os is ' + os.type() });
 });
 
 module.exports = router;
