085b9757fbe8654c50ffa2a2c842055fb6a5718b
kehinde <kehindesalaudeen222@gmail.com>
2021-02-18 16:29:59 +0100

    check if user exist

--- routes/calibre/calibre.js
+++ routes/calibre/calibre.js
@@ -1,30 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var {Client}  = require('pg');
-/*
+
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false
     }
   });
-  const table = "SELECT table_schema,users FROM information_schema.tables;"
-  client.connect();
-  client.query(table, (err, res) => {
-    router.get('/db',(req,res)=>{
-        if (err) {
-            console.error(err);
-            res.send(err)
-            return;
-        }
-    })
-    console.log('Table is successfully created');
-    res.send("table created");
-    client.end();
-});
-*/
+//   const table = "SELECT table_schema,users FROM information_schema.tables;"
+//   client.connect();
+//   client.query(table, (err, res) => {
+//     router.get('/db',(req,res)=>{
+//         if (err) {
+//             console.error(err);
+//             res.send(err)
+//             return;
+//         }
+//     })
+//     console.log('Table is successfully created');
+//     res.send("table created");
+//     client.end();
+// });
+
 router.get("/item",function(req,res,next){
-    res.send("you are on " + req.originalUrl + "\t API");
+    res.send("you are on " + req.originalUrl + "\t API" + "--------- conn \n " + process.env.DATABASE_URL);
 })
 router.get("/add",function(req,res,next){
     res.send("you are on " + req.originalUrl + "\t API");
